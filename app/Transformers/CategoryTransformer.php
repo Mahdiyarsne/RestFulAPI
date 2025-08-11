6bec9bef8d28f1be8f675fa1b6d59124d11fd4a4
Mahdiyar <mahdiyarsne98@gmail.com>
2025-08-11 13:57:48 +0330

    بخش hateoas

--- app/Transformers/CategoryTransformer.php
+++ app/Transformers/CategoryTransformer.php
@@ -39,6 +39,35 @@ class CategoryTransformer extends TransformerAbstract
             'creationDate' => (string) $category->created_at,
             'lastChange' => (string)$category->updated_at,
             'deletedDate' => isset($category->deleted_at) ? (string) $category->deleted_at : null,
+
+            'links' => [
+                [
+                    'rel' => 'self',
+                    'href' => route('categories.show', $category->id)
+                ],
+
+                [
+                    'rel' => 'category.buyers',
+                    'href' => route('categories.buyers.index', $category->id)
+                ],
+
+                [
+                    'rel' => 'category.products',
+                    'href' => route('categories.products.index', $category->id)
+                ],
+
+                [
+                    'rel' => 'category.sellers',
+                    'href' => route('categories.sellers.index', $category->id)
+                ],
+
+
+                [
+                    'rel' => 'category.transactios',
+                    'href' => route('categories.transactions.index', $category->id)
+                ],
+            ]
+
         ];
     }
 
